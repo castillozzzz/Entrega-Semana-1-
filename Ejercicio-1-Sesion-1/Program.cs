022e526b5c1c2f316b9ee77b046069585160f30d
castillozzzz <josueisaireyes@gmail.com>
2025-08-31 00:01:09 -0600

    Add files via upload

---
+++ Ejercicio-1-Sesion-1/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class ProgramaFinca
+{
+    static void Main()
+    {
+        // Mensaje de bienvenida
+        Console.WriteLine("=== Calculadora de área en manzanas ===");
+        Console.WriteLine("Este programa calcula el área de un terreno rectangular en manzanas.");
+        Console.WriteLine("El ancho está fijo en 5 varas.\n");
+
+        // Pedir la longitud al usuario
+        Console.Write("Ingrese la longitud del terreno en varas: ");
+        double longitud = Convert.ToDouble(Console.ReadLine());
+
+        // Definir el ancho fijo en varas
+        double ancho = 5;
+
+        // Calcular el área en varas²
+        double areaVaras2 = longitud * ancho;
+
+        // Convertir a manzanas (1 manzana = 1428.8 varas²)
+        double areaManzanas = areaVaras2 / 1428.8;
+
+        // Mostrar resultados
+        Console.WriteLine("\n--- Resultados ---");
+        Console.WriteLine("Área en varas²: " + areaVaras2);
+        Console.WriteLine("Área en manzanas: " + areaManzanas);
+
+        Console.WriteLine("\n¡Cálculo completado!");
+    }
+}
---
+++ Ejercicio-3-Sesion-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class ProgramaCiclista
+{
+    static void Main()
+    {
+        // Declaración de variables
+        double velocidad, tiempo, distancia;
+
+        // Pedir la velocidad promedio
+        Console.Write("Ingrese la velocidad promedio en km/h: ");
+        velocidad = Convert.ToDouble(Console.ReadLine());
+
+        // Pedir el tiempo recorrido
+        Console.Write("Ingrese el tiempo recorrido en horas: ");
+        tiempo = Convert.ToDouble(Console.ReadLine());
+
+        // Calcular la distancia recorrida
+        distancia = velocidad * tiempo;
+
+        // Mostrar el resultado
+        Console.WriteLine("La distancia total recorrida es: " + distancia + " km");
+    }
+}
